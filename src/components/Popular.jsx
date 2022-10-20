94a689576293c613b8f767f1012e737f820faec6
Jason Gutierrez <jason.gutierrez0302@gmail.com>
2022-10-20 13:13:53 +0800

    oct 20 2022 second commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -14,17 +14,17 @@ function Popular() {
 
     const getPopular = async () => {
 
-        const check = localStorage.getItem('popular');
+        //const check = localStorage.getItem('popular');
 
-        if(check){
-            setPopular(JSON.parse(check))
-        }else{
+        //if(check){
+            //setPopular(JSON.parse(check))
+        //}else{
             const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
             const data = await api.json();
             localStorage.setItem('popular', JSON.stringify(data.recipes));
             console.log(data);
             setPopular(data.recipes);
-        }
+        //}
 
         
         
